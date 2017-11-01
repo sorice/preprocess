39807fa8e947458021a285bdb85610827ca5b277
Abel Meneses Abad <abelma1980@gmail.com>
2017-10-31 22:56:20 -0400

    [dev][ner] Adding interface & multioutput parameters to NER function. This 	change allows to change the NLP API to get results, and to change 	the format of the output for future experiments with textsim pack.

--- preprocess/deep/techniques.py
+++ preprocess/deep/techniques.py
@@ -34,27 +34,62 @@ stanford_ner_dir = os.path.relpath(config['NER']['stanford_ner_dir'][2:])
 stanford_ner_model['en'] = os.path.relpath(stanford_ner_dir[:-1] + config['NER']['stanford_ner_eng_model'][2:-1])
 stanford_ner_jar = os.path.relpath(stanford_ner_dir[:-1]+config['NER']['stanford_ner_jar'][2:-1])
 
-def ner(text, lang='en'):
+def ner(text, lang='en', interface='stanford', multioutput='raw_value'):
     """Name Entity Recognition.
 
-    :Model:
+     Parameters
+    ----------
+    text: string to parse, generally a sentence.
 
-    StanfordNERTagger
+    lang: natural languaje of the text.
+
+    interface: a tag of one of the implemented interfaces in preprocess.
+
+    multioutput: string in ['raw_value', 'tuple_list', 'raw_tag']
+                 Format type of the output.
+                 Tuple list format is implemented for ngram generalization of
+                 some token distances in textsim papckage.
+
+    Returns
+    -------
+
+    parsed result : string output, list of tuples [(token, NE tag)].
 
     :Explanation:
 
     The returned string structure is build to use textsim string and token distances.
 
     """
+    if interface == 'stanford':
+        result = __stanford_ner(text, lang, multioutput)
+    if interface == 'freeling':
+        result = ''
+
+    return result
+
+def __stanford_ner(text,lang='en',multioutput='raw_value'):
+    """Interface for NLTK Stanford Name Entity Tagger interface.
+    """
+
     st = StanfordNERTagger(stanford_ner_model[lang], stanford_ner_jar,'utf8')
-    result = st.tag(text.split())
+    tuple_list = st.tag(text.split())
     string = ''
-    for (word,tag) in result:
+    raw_tag = ''
+    for (word,tag) in tuple_list:
         if tag == 'O':
-            string += word+' '
+            string += word +' '
+            raw_tag += word + ' '
         else:
-            string += word+'/'+tag+' '
-    return string
+            string += word +'/'+tag+' '
+            raw_tag += tag + ' '
+
+    if isinstance(multioutput, str):
+        if multioutput == 'raw_value':
+            return string
+        if multioutput == 'tuple_list':
+            return tuple_list
+        if multioutput == 'raw_tag':
+            return raw_tag
 
 if __name__ == '__main__':
     s1=input("Input text A:")
